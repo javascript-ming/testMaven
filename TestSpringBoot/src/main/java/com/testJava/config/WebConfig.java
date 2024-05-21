7cab8a3fe06b9e3acc8c200df003d6433c6a7154
frady-9020M <wujixmm@gmail.com>
2024-05-21 11:59:55 +0800

    feat: 新增跨域配置

---
+++ TestSpringBoot/src/main/java/com/testJava/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.testJava.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 允许跨域的路径
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*")
+                .exposedHeaders("*")
+                .maxAge(6000);
+
+    }
+}
